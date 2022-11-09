720017bb619266f0566f1d45a982ab6b8f5aa809
eads72 <96619838+eads72@users.noreply.github.com>
2022-11-09 21:52:41 +0100

    card class and methods added

--- app.js
+++ app.js
@@ -3,12 +3,63 @@
 //create flipping effect on click
 //
 
-const anArray = [1, 2, 3, 4, 5,]
+const anArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const cardsDiv = document.getElementById("cards-div");
+let arrayOfCards = [];
+
+
+
+class Card {
+    constructor(id) {
+      this.id = id;
+      this.isVisible = false;
+      this.isFound = false;
+    }
+    setVisibility(boolean){
+        this.isVisible = boolean;
+    }
+    makeFound(){
+        this.isFound = true;
+    }
+};  
+
+for (let i = 1; i < 11; i++) {
+    let newCard = new Card(i);
+    arrayOfCards.push(newCard);
+}
+
+
+
+//takes an array as parameter, doubles the array
+function doubleArray(theArray){
+    let newArray = theArray.concat(theArray);
+    return newArray
+};
+
+// creates the division for one card. Passes a number as a parameter and returns the card
+function makeCardDiv(num){
+    let card = document.createElement('div');
+    card.classList.add('card', num);
+    card.textContent = num;
+    cardsDiv.appendChild(card);
+    return card
+};
+
+// Takes two cards as paramters,
+// If they have the same id, makes them found, else, sets them invisible
+function compareCards(card1, card2){
+    if(card1.id==card2.id){
+        card1.makeFound;
+        card1.makeFound;
+    }else{
+        card1.setVisibility(false);
+        card2.setVisibility(false);
+    }
+}
 
 function getRandomInt(max = 10){
     return Math.floor(Math.random() *max)
 };
-
 
 // takes an array as a parameter, returns the shuffled array
 function shuffle(theArray){
@@ -25,4 +76,12 @@ function shuffle(theArray){
     return shuffledArray
 };
 
-console.log(shuffle(anArray));
+let shuffledArray = shuffle(doubleArray(anArray));
+
+
+shuffledArray.forEach(makeCardDiv);
+let myCard = new Card(2);
+console.log(myCard.id);
+console.log(myCard.isFound);
+console.log(myCard.isVisible);
+console.log(arrayOfCards);
